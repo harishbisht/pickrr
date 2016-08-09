9b4ec76989329701c5faf928cf9a5031a75be2b2
Harish Bisht <harishbisht092@gmail.com>
2016-08-09 13:08:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pickrr',
+      version='0.1',
+      description='Pickrr order placing, order cancellation and order tracking api',
+      url='http://github.com/harishbisht/pickrr',
+      author='Harish Bisht',
+      author_email='harish@pickrr.com',
+      license='MIT',
+      packages=['pickrr'],
+      zip_safe=False)
