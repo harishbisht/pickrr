fbfe8cae4a52e0294aae90a2f9ed11a3308bd282
Harish Bisht <harishbisht092@gmail.com>
2016-08-11 00:07:28 +0530

    version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pickrr',
       version='0.3',
-      description='Pickrr order placing, order cancellation and order tracking api',
+      description='Pickrr order placing, order Cancellation and order tracking api',
       url='http://github.com/harishbisht/pickrr',
       author='Harish Bisht',
       author_email='harish@pickrr.com',
