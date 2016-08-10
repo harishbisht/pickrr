63661dc985a0bc4fb3461b4648d8a61e53625d62
Harish Bisht <harishbisht092@gmail.com>
2016-08-11 00:26:23 +0530

    simple fix

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='pickrr',
-      version='0.3',
+      version='0.4',
       description='Pickrr order placing, order Cancellation and order tracking api',
       url='http://github.com/harishbisht/pickrr',
       author='Harish Bisht',
       author_email='harish@pickrr.com',
       license='MIT',
       packages=['pickrr'],
+      install_requires=[
+          'requests',
+      ],
       zip_safe=False)
